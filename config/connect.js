a71b172e16856287a3c626ac603e0edb5984de07
JaaDev2060 <144540135+JaaDev2060@users.noreply.github.com>
2023-09-13 03:50:42 +0700

    Add files via upload

--- config/connect.js
+++ config/connect.js
@@ -1,4 +1,6 @@
 require('./settings')
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
+
 const { default: comotconnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require('@adiwajshing/baileys')
 const { state } = useSingleFileAuthState(`./sessi.json`)
 const pino = require('pino')
@@ -82,6 +84,50 @@ comot.ev.on('contacts.update', update => {
 for (let contact of update) {
 let id = comot.decodeJid(contact.id)
 if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
+}
+})
+
+comot.ev.on('group-participants.update', async (anu) => {
+if (!wlcm.includes(anu.id)) return
+console.log(anu)
+try {
+let metadata = await comot.groupMetadata(anu.id)
+let participants = anu.participants
+for (let num of participants) {
+// Get Profile Picture User
+try {
+ppuser = await comot.profilePictureUrl(num, 'image')
+} catch {
+ppuser = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+}
+
+// Get Profile Picture Group
+try {
+ppgroup = await comot.profilePictureUrl(anu.id, 'image')
+} catch {
+ppgroup = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+}
+
+if (anu.action == 'add') {
+comot.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `Haii Kak Kontol *@${num.split("@")[0]}* Selamat Datang Di Group *${metadata.subject}* 👋
+ ▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+Terima Kasih Sudah Bergabung Jangan Lupa Baca Deskripsi Yah
+▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+Creator : https://wa.me/6283838077485`})
+} else if (anu.action == 'remove') {
+comot.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `Karena Untuk Setiap Ucapan Selamat Datang Akan Selalu Diakhiri Dengan Ucapan Selamat Tinggal Jangan Kembali Lagi Ya Anak Anjeang👉😂👈
+▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+Selamat Tinggal *@${num.split("@")[0]}* Di Group *${metadata.subject}*
+▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+Creator : https://wa.me/6285711324080`})
+} else if (anu.action == 'promote') {
+comot.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `@${num.split('@')[0]} Ciee Jadi Admin Di Group Wkwkwk Bisa Kali Traktir Bakso Uhuy🤧 ${metadata.subject} ${metadata.desc}`  })
+} else if (anu.action == 'demote') {
+comot.sendMessage(anu.id, { image: { url: ppuser }, mentions: [num], caption: `@${num.split('@')[0]} Wkwkwkwk Mampus Lu Di Turunin Bukan Jadi Admin Lagi Wkwkkwwkkw ${metadata.subject} ${metadata.desc}`})
+  }
+}
+} catch (err) {
+console.log(err)
 }
 })
 
